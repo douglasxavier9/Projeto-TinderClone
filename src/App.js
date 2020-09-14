e99093a91d60081f10fac6f817ca24d846d3a209
Douglas Xavier <douglasxavierr1999@gmail.com>
2020-09-13 23:01:13 -0300

    update 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World, my first project in react </h1>
+
+      {/*Header*/}
+      <Header />
+      
+      {/*Tinder Cards*/}
+      {/*Buttons below tinder cards*/}
+
+
+      {/*Chat*/}
+      {/*Individual chat screen*/}
+
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        ///BEM
+        <div className="header">
+            <PersonIcon  className="header__icon" fontSize="large"/>
+            <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt=" Tinder logo"/>
+            <ForumIcon className="header__icon" fontSize="large"/> 
+        </div>
+    )
+}
+
+export default Header;
